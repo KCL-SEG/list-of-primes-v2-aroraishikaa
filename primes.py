11d97eb71127fc07703ad0b8bbc8fbe1a1154c88
Ishika Arora <ishika.arora@kcl.ac.uk>
2023-12-08 15:47:07 +0000

    Complete task

--- primes.py
+++ primes.py
@@ -1,6 +1,15 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+
+    list_of_primes = []
+    num = 2  # Starting from the first prime number
+
+    while len(list_of_primes) < number_of_primes:
+        if all(num % i != 0 for i in range(2, int(num**0.5) + 1)):
+            list_of_primes.append(num)
+        num += 1
+
+    return list_of_primes
+
+
